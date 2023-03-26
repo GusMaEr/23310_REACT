d177ebcafde638e7f57d75aec8237198d589f140
Gustavo ERNESTO <102641696+GusMaEr@users.noreply.github.com>
2023-03-26 20:46:22 -0300

    2

---
+++ Practica JS/JS/index.js
@@ -0,0 +1,140 @@
+// //Variables "let variable modificable y const variable fija no modificable"
+
+// const miVariable = 1
+// console.log(miVariable)
+
+// //operadores aritmeticos "+,-,*./"
+
+// //operadores logicos "&& y ||"
+
+// //Operadores de comparacion "==, ===, <, >, <=, >=,!=="
+
+// //1. Crear una variable llamada miNombre y guardar en ella su primer nombre
+
+// const miNombre='Gustavo'
+
+// //2. Crear una variable llamda miApellido y guardarla en ella su apellido
+// const miApellido='ERNESTO'
+
+// //3. Crear una variable llamda miEdad y guardarla en ella su edad
+// let miEdad="30"
+
+// //5. visuaizar  todas las variable dentro de la consola del navegador escribiendo cada uno de ellas
+// console.log('Mi nombre es:'+ miNombre + ' '+ miApellido)
+
+// Crear una variable sumaEdades, restaEdades, productoEdades, divisionEdades y 
+//calcular sus respectivas operaciones con las variables miEdad y edadMascota.
+// let miEdad = 40
+// let edadMascota = 10
+// let sumaEdades = miEdad + edadMascota;
+// let restaEdades = miEdad - edadMascota;
+// let productoEdades = miEdad * edadMascota;
+// let divisionEdades = miEdad / edadMascota;
+
+// console.log('La suma de las edades es: '+ sumaEdades)
+// console.log('La resta de las edades es: '+ restaEdades)
+// console.log('El producto de las edades es: '+ productoEdades)
+// console.log('La division de las edades es: '+ divisionEdades)
+
+// Realizar la carga de las variables del punto 1 al 5 utilizando prompt() 
+// y luego ejecutar el código correspondiente para 
+// realizar los puntos del 6 al 9 con los datos cargados.
+// let miNombre = prompt("Ingrese nombre")
+// let miApellido = prompt("Ingrese nombre")
+// let miMascota = prompt("Ingrese nombre Mascota")
+// console.log(miNombre, miApellido, miMascota)
+
+//Crear un objeto llamado alumno con un mínimo de 5 propiedades, 
+//mostrar dicho objeto utilizando console.table()
+
+// const alumno = {nombreAlumno:"SAPO", 
+//                 apellidoAlumno:"PEPE", 
+//                 edadAlumno:"13", 
+//                 cursoAlumno:"4",altura:"1.80 mtrs"};
+// console.table(alumno)
+
+//Crear un array llamado frutas con un mínimo de 5 elementos 
+//y mostrar por consola el array completo y cada uno de los elementos por separado.
+
+// let frutas = ["pera","manzana","uva","naranja","ciruela"]
+// console.log(frutas)
+// console.log(frutas[0])
+// console.log(frutas[1])
+// console.log(frutas[2])
+// console.log(frutas[3])
+// console.log(frutas[4])
+
+//Realizar un programa que dado 2 objetos representando personas con las propiedades nombre, 
+//edad y altura imprima por pantalla cual de las 2 personas es la mas alta y cual es la de mayor edad.
+// let persona1={
+//     nombre:"Pepe",
+//     edad:20,
+//     altura:1,
+//             } 
+
+// let persona2={
+//     nombre:"Sapo",
+//     edad:40,
+//     altura:2,
+//             }
+
+// if (persona1.altura > persona2.altura){
+//     if (persona1.edad > persona2.edad){
+//         console.log("la persona 1 es mas grande y mayor")
+//     } else {console.log("la persona 1 es mas grande y no es mayor")}
+// console.log("la persona 1 es menor que persona 2")
+// }
+
+// let c= 1
+// let d= 2
+// function prueba(c,d){
+//     if (c > d){
+//         console.log("1")
+//      } else if (c < d){
+//         console.log("2")
+//      } else {
+//         console.log("3")
+//      }
+//     }
+
+// prueba(c,d);
+
+//  let persona1={
+//      nombre:"Pepe",
+//      edad:20,
+//      altura:1,} 
+
+//  let persona2={
+//      nombre:"Sapo",
+//      edad:40,
+//      altura:2,}
+
+// let persona3={
+// nombre:"JUFA",
+// edad:persona1.edad,
+// }
+    
+// function comparador(persona1, persona2){
+//     if (persona1.edad < persona3.edad){
+//         console.log("no son iguales")
+//     } else {console.log("es mayor")}
+// }
+// comparador(persona1, persona2)
+
+// let arreglo=[1,2,3,4,5]
+
+// function muestraArreglo(){
+//     console.log(arreglo[3]) }
+// muestraArreglo()
+
+// let arreglo=[1,2,3,4,5]
+// function muestraArreglo(){
+//     console.log(arreglo)
+// }
+// muestraArreglo()
+
+// let cambioArreglo = arreglo.map(function(datos){
+//     return datos;
+// })
+// console.log(cambioArreglo)
+
