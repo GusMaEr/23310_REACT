c5527d0cbdd472b8bf515d41c851d79f2ab30d5b
Gustavo ERNESTO <102641696+GusMaEr@users.noreply.github.com>
2023-03-26 20:25:03 -0300

    PrimerApp

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import PrimerComponents from './components/PrimerComponentes'
+
+function App() {
+  return (
+    <PrimerComponents/>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/PrimerComponentes.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export const PrimerComponentes = () => {
+  return (
+    <div>
+      <h1>!!!! Hola Mundo React !!!!</h1>
+      </div>
+  )
+}
+export default PrimerComponentes
