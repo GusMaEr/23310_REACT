213e794bd7e410e8941108db1324d1d4db24f6b2
Gustavo ERNESTO <102641696+GusMaEr@users.noreply.github.com>
2023-04-10 18:59:15 -0300

    20230410

--- my-app/src/components/Inicio.jsx
+++ my-app/src/components/Inicio.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 const Inicio = () => {
   return (
      <div>
--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const navbarPrincipal = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
             <Nav.Link as={Link} to="/Agendas"></Nav.Link>
             <Nav.Link as={Link} to="/Cuadernos"></Nav.Link>
             <Nav.Link as={Link} to="/Calendarios"></Nav.Link>
